6d4168dd6f178fecb63eddc4682516fea4fe82cf
Manasi Patil <61625157+manasipatil0109@users.noreply.github.com>
2021-10-12 15:35:26 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,41 @@
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+});
+
+
+// digital
+setInterval(function(){
+    const clock = document.querySelector(".display");
+    let time = new Date();
+    let sec = time.getSeconds();
+    let min = time.getMinutes();
+    let hr = time.getHours();
+    let day = 'AM';
+    if(hr > 12){
+      day = 'PM';
+      hr = hr - 12;
+    }
+    if(hr == 0){
+      hr = 12;
+    }
+    if(sec < 10){
+      sec = '0' + sec;
+    }
+    if(min < 10){
+      min = '0' + min;
+    }
+    if(hr < 10){
+      hr = '0' + hr;
+    }
+    clock.textContent = hr + ':' + min + ':' + sec + " " + day;
+  });
